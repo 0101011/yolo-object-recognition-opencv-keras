c4e4fafb42ca56b6d3736cdcfdf4e62122d457b4
0101011 <andrew.stepin@gmail.com>
2019-09-15 01:30:11 +0300

    redundant spaces

--- yolo.py
+++ yolo.py
@@ -6,16 +6,12 @@ import os
 
 # Argument parser for input and parameters.
 ap = argparse.ArgumentParser()
-
 ap.add_argument("-i", "--image", required=True,
 	            help="Path to an input image")
-
 ap.add_argument("-y", "--yolo", required=True,
 	            help="Base path to YOLO folder")
-
 ap.add_argument("-c", "--confidence", type=float, default=0.5,
 	            help="Minimum probability to filter weak detections")
-
 ap.add_argument("-t", "--threshold", type=float, default=0.3,
 	            help="Threshold when applying non-maxima suppression")
 
